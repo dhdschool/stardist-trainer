da133ec0a355e58d8e8174b722e2bfe4536123eb
dhdschool <dhdschool@outlook.com>
2025-03-08 12:29:13 -0600

    Start work on model

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 import panel as pn
 import sys
+from model import StarDistAPI
 #import widgets
 from pathlib import Path
 
@@ -9,42 +10,90 @@ def exit(event):
     if event:
         sys.exit()
 
+def train(event):
+    if event:
+        img_fp = image_dir_widget.value
+        model_fp = model_dir_widget.value
+        if img_fp is None or model_fp is None:
+            return
+        
+        img_fp = img_fp[0]
+        model_fp = model_fp[0]
+        
+        image_dir = Path(img_fp)
+        model_dir = Path(model_fp)
+        
+        epochs = epoch_input.value
+        batch_size = batch_input.value
+        validation_percentage = val_input.value
+        image_format = image_format_selector.value
+        mask_format = mask_format_selector.value
+        imagej = ij_switch.value
+        overwrite = model_ow_switch.value
+        
+        classes = None if co_classes_input.value == 1 else co_classes_input.value
+        config = {
+            'axes': co_axes_selector.value[:2],
+            'n_rays': co_nrays_input.value,
+            'n_channel_in': co_channels_input.value,
+            'n_classes': classes
+        }
+        
+        model = StarDistAPI(image_dir,
+                            model_dir,
+                            epochs,
+                            validation_percentage,
+                            image_format,
+                            mask_format,
+                            imagej,
+                            overwrite,
+                            config_kwargs=config,
+                            batch_size=batch_size)
+        
+        train_gen = model.train()
+        loadingbar_widget.active = True
+        for progress in train_gen:
+            loadingbar_widget.value = int(round(progress * 100))
+        loadingbar_widget.active = False
+        
+
 def exclusive(target, event):
     target.value = not event.new
 
 sidebar_widgth = 300
 
-image_dir_widget = pn.widgets.FileInput(directory=True)
+image_dir_widget = pn.widgets.FileSelector(file_pattern='', root_directory=str(Path.home()), only_files=False)
 image_text_widget = pn.widgets.StaticText(name="Image Directory")
 image_widget = pn.Column(image_text_widget, image_dir_widget)
 
 model_text_widget = pn.widgets.StaticText(name="Model Directory")
-model_dir_widget = pn.widgets.FileInput(directory=True)
+model_dir_widget = pn.widgets.FileSelector(file_pattern='', root_directory=str(Path.home()), only_files=False)
 model_widget = pn.Column(model_text_widget, model_dir_widget)
 
-filedir = pn.Row(image_widget, model_widget)
+filedir = pn.Column(image_widget, pn.Spacer(height=50), model_widget)
 
 
 exit_widget = pn.widgets.Button(name='Server Shutdown', button_type='danger')
 train_widget = pn.widgets.Button(name='Train Model', button_type='primary')
-
+loadingbar_widget = pn.widgets.Progress(max=100, bar_color='success')
 
 with open(Path('md') / Path('info.md')) as file:
     info = file.read()
 info_widget = pn.pane.Markdown(info)
 
 pn.bind(exit, exit_widget, watch=True)
+pn.bind(train, train_widget, watch=True)
 
 vgg_switch = pn.widgets.Switch(value=False, width=40)
-vgg_text = pn.widgets.StaticText(name='VGG VIA Format')
+vgg_text = pn.widgets.StaticText(name='VGG VIA Format', align='center')
 vgg_widget = pn.layout.Row(vgg_text, vgg_switch, sizing_mode='fixed', width=sidebar_widgth)
 
 ij_switch = pn.widgets.Switch(value=True, width=40)
-ij_text = pn.widgets.StaticText(name='ImageJ Format')
+ij_text = pn.widgets.StaticText(name='ImageJ Format', align='center')
 ij_widget = pn.layout.Row(ij_text, ij_switch, sizing_mode='fixed', width=sidebar_widgth)
 
 model_ow_switch = pn.widgets.Switch(value=False, width=40)
-model_ow_text = pn.widgets.StaticText(name='Overwrite Model Config')
+model_ow_text = pn.widgets.StaticText(name='Overwrite Model Config', align='center')
 model_ow_widget = pn.Row(model_ow_text, model_ow_switch, sizing_mode='fixed', width=sidebar_widgth)
 
 ij_switch.link(vgg_switch, callbacks={'value':exclusive})
@@ -52,25 +101,55 @@ vgg_switch.link(ij_switch, callbacks={'value':exclusive})
 
 
 val_input = pn.widgets.IntInput(value=20, start=1, end=100)
-val_text = pn.widgets.StaticText(name="Validation split percentage")
+val_text = pn.widgets.StaticText(name="Validation split percentage", align='center')
 val_widget = pn.Row(val_text, val_input, sizing_mode='fixed', width=sidebar_widgth)
 
 epoch_input = pn.widgets.IntInput(value=1, start=1)
-epoch_text = pn.widgets.StaticText(name="Epochs")
+epoch_text = pn.widgets.StaticText(name="Epochs", align='center')
 epoch_widget = pn.Row(epoch_text, epoch_input, sizing_mode='fixed', width=sidebar_widgth)
+
+batch_input = pn.widgets.IntInput(value=4, start=1)
+batch_text = pn.widgets.StaticText(name="Batch Size", align='center')
+batch_widget = pn.Row(batch_text, batch_input, sizing_mode='fixed', width=sidebar_widgth)
 
 coord_formats = [
     "XYC",
     "YXC"
 ]
 
-image_format_text = pn.widgets.StaticText(name="Image coord format")
+image_format_text = pn.widgets.StaticText(name="Image coord format", align='center')
 image_format_selector = pn.widgets.Select(options=coord_formats)
 image_format_widget = pn.Row(image_format_text, image_format_selector, sizing_mode='fixed', width=sidebar_widgth)
 
-mask_format_text = pn.widgets.StaticText(name="Mask coord format")
+mask_format_text = pn.widgets.StaticText(name="Mask coord format", align='center')
 mask_format_selector = pn.widgets.Select(options=coord_formats)
 mask_format_widget = pn.Row(mask_format_text, mask_format_selector, sizing_mode='fixed', width=sidebar_widgth)
+
+config_overwrite_width = 200
+co_axes_text = pn.widgets.StaticText(name="axes", align='center')
+co_axes_selector = pn.widgets.Select(options=list(reversed(coord_formats)))
+co_axes_widget = pn.Row(co_axes_text, co_axes_selector, sizing_mode='fixed', width=config_overwrite_width)
+
+co_nrays_text = pn.widgets.StaticText(name="n_rays", align='center')
+co_nrays_input = pn.widgets.IntInput(value=32, start=1)
+co_nrays_widget = pn.Row(co_nrays_text, co_nrays_input, sizing_mode='fixed', width=config_overwrite_width)
+
+co_channels_text = pn.widgets.StaticText(name="n_channels_in", align='center')
+co_channels_input = pn.widgets.IntInput(value=3, start=1)
+co_channels_widget = pn.Row(co_channels_text, co_channels_input, sizing_mode='fixed', width=config_overwrite_width)
+
+co_classes_text = pn.widgets.StaticText(name="n_classes", align='center')
+co_classes_input = pn.widgets.IntInput(value=1, start=1)
+co_classes_widget = pn.Row(co_classes_text, co_classes_input, sizing_mode='fixed', width=config_overwrite_width)
+
+config_overwrite_data = pn.layout.Column(
+    co_axes_widget,
+    co_nrays_widget,
+    co_channels_widget,
+    co_classes_widget
+)
+
+config_overwrite_dropdown = pn.layout.Accordion(("Config Overwrite Dropdown", config_overwrite_data))
 
 
 toggles = pn.Column(ij_widget,
@@ -78,18 +157,24 @@ toggles = pn.Column(ij_widget,
                     model_ow_widget)
 
 hyperparameters = pn.Column(epoch_widget,
+                            batch_widget,
                             val_widget,
                             image_format_widget,
                             mask_format_widget)
 
 mainbar = pn.Column(filedir,
+                    pn.Spacer(height=20),
+                    loadingbar_widget,
                     train_widget, 
                     exit_widget)
 
 sidebar = pn.Column(info_widget, 
-                    pn.Spacer(width=200),
+                    pn.Spacer(height=20),
                     hyperparameters, 
-                    toggles)
+                    toggles,
+                    pn.Spacer(height=20),
+                    config_overwrite_dropdown,
+                    pn.Spacer(height=50))
 
 app = pn.template.MaterialTemplate(
     title='Stardist Trainer',
--- src/model.py
+++ src/model.py
@@ -7,12 +7,186 @@ import os
 import json
 from matplotlib import pyplot as plt
 from csbdeep.utils import normalize
-
-#### Training TUI
-class AnnotatedLoader():
+#TODO: ImageJ loader
+class StarDistAPI:
     def __init__(self,
-                 image_dir=Path('annotated-images') / Path('alloted-images'),
-                 csv_dir = Path('annotated-images') / Path('csv-files'),
+                 image_dir,
+                 model_dir,
+                 epochs=1,
+                 batch_size=4,
+                 val_per=20,
+                 image_format='XYC',
+                 mask_format='XYC',
+                 imagej=True,
+                 overwrite=False,
+                 config_kwargs={},
+                ):
+        
+        image_dir = Path(image_dir).absolute()
+        self.image_dir = Path(image_dir) / Path('images')
+        self.mask_dir = Path(image_dir) / Path('masks')
+        self.csv_dir = Path(image_dir) / Path('csv')
+        
+        model_dir = Path(model_dir).absolute()
+        model_name = model_dir.name
+        model_dir = model_dir.parent
+        
+        config = Config2D(**config_kwargs) if overwrite else None
+        
+        self.model = StarDist2D(config=config,
+                                name=model_name,
+                                basedir=model_dir)
+        
+        self.epochs = epochs
+        self.val_size = int(round((val_per / 100) * batch_size, 0))
+        self.image_format = image_format
+        self.mask_format = mask_format
+        self.imagej = imagej
+        self.batch_size = batch_size
+
+        self.model.prepare_for_training()
+        
+    def train(self):
+        if self.imagej:
+            pass
+        if not self.imagej:
+            loader = VGGLoader(self.image_dir, self.csv_dir)
+            
+        dataset = loader.data()
+        length = len(loader)
+        
+        X_train, y_train = [], []
+        X_val, y_val = [], []
+        for X, y in dataset:
+            if len(X_val) < self.val_size:
+                X_val.append(X), y_val.append(y)
+            else:
+                X_train.append(X)
+                y_train.append(y)
+            
+            if len(X_train) == self.batch_size:
+                
+                X_arr = np.array(X_train)
+                X_val_arr = np.array(X_val)
+                if self.model.config.axes != self.image_format:
+                    X_arr = X_arr.transpose(axes=(1, 0, 2))
+                    X_val_arr = X_val_arr.transpose(axes=(1, 0, 2))
+
+                y_arr = np.array(y_train)
+                y_val_arr = np.array(y_val)
+                if self.model.config.axes != self.mask_format:
+                    y_arr = y_arr.transpose(axes=(1, 0, 2))
+                    y_val_arr = y_val_arr.transpose(axes=(1, 0, 2))
+                
+                X_train = []; y_train = []
+                X_val = []; y_val = []
+                
+                self.model.train(X_arr, y_arr, validation_data=(X_val_arr, y_val_arr), epochs=self.epochs)
+                yield len(loader) / length # Percentage of training done
+class Loader:
+    def __init__(self,
+                 image_dir,
+                 label_dir,
+                 memory_limit=32):
+        self.image_dir = image_dir
+        self.memory_limit = memory_limit
+        
+        self.image_paths = list(map(lambda x: str(self.image_dir / Path(x)),
+                                    os.listdir(self.image_dir)))
+        
+        self.image_len = (0, min(self.memory_limit, len(self.image_paths)))
+        self.length = len(self.images_paths)
+        
+        self.images = {}
+        self.labels = {}
+        
+        self._load_images()
+    
+    def _load_images(self):
+        for index in range(*self.image_len):
+            name = self.image_paths[index].stem
+            
+            with Image.open(name) as img:
+                img_rgb = img.convert('RGB')
+                img_rgb = np.array(img_rgb)
+                img_rgb = normalize(img_rgb, 1, 99.8)
+                self.images[name] = img_rgb
+                self._load_label(name)
+            
+    def _load_label(self, name):
+        pass
+    
+    def data(self):
+        for index in range(len(self.image_paths)):
+            name = self.image_paths[index]
+            
+            if len(self.images) == 0 and index != len(self.image_paths) - 1:
+                self.image_len = (self.image_len[1]+1, min(self.image_len[1] + self.memory_limit, len(self.image_paths) - self.image_len[1]))
+                self._load_images()
+            
+            img, lbl = self.images.pop(name), self.labels.pop(name)
+            self.length -= 1
+            
+           
+            yield img, lbl
+    
+ 
+class ImageJLoader():
+    def __init__(self,
+                 image_dir,
+                 mask_dir,
+                 memory_limit=32):
+        
+        self.image_dir = image_dir
+        self.mask_dir = mask_dir
+        self.memory_limit = memory_limit
+
+        self.image_paths = list(map(lambda x: str(self.image_dir / Path(x)),
+                                    os.listdir(self.image_dir)))
+
+        self.mask_paths = list(map(lambda x: str(self.image_dir / Path(x), 
+                                                 os.listdir(self.image_dir))))  
+        
+        self.image_len = (0, min(self.memory_limit, len(self.image_paths)))
+        self.length = len(self.images_paths)
+        
+        self.images = {}
+        self.labels = {}
+        
+        self._load_images()
+        
+    def _load_images(self):
+        for index in range(*self.image_len):
+            name = self.image_paths[index]
+            
+            with Image.open(name) as img:
+                img_rgb = img.convert('RGB')
+                img_rgb = np.array(img_rgb)
+                img_rgb = normalize(img_rgb, 1, 99.8)
+                self.images[name] = img_rgb
+            
+   
+    def data(self):
+        for index in range(len(self.image_paths)):
+            name = self.image_paths[index]
+            
+            if len(self.images) == 0 and index != len(self.image_paths) - 1:
+                self.image_len = (self.image_len[1]+1, min(self.image_len[1] + self.memory_limit, len(self.image_paths) - self.image_len[1]))
+                self._load_images()
+            
+            img, lbl = self.images.pop(name), self.labels.pop(name)
+            self.length -= 1
+            
+           
+            yield img, lbl
+                
+    def __len__(self):
+        return self.length
+        
+class VGGLoader():
+    def __init__(self,
+                 image_dir,
+                 csv_dir,
                  from_csv=True,
                  memory_limit=32):
         
@@ -25,11 +199,12 @@ class AnnotatedLoader():
                                     os.listdir(self.image_dir)))
                 
         self.image_len = (0, min(self.memory_limit, len(self.image_paths)))
+        self.length = len(self.images_paths)
         
         self.images = {}
         self.labels = {}
-        self.counts = {}
-        self.give_counts = False
+        #self.counts = {}
+       # self.give_counts = False
         
         self._load_images()
         
@@ -46,9 +221,9 @@ class AnnotatedLoader():
                 if self.from_csv:
                     csv_path = self.csv_dir / Path(name).with_suffix('.csv').name
                     label = self._csv_to_label_mask(csv_path, img.size)
-                    count = pd.read_csv(csv_path)['region_count'].head(1).squeeze()
+                    #count = pd.read_csv(csv_path)['region_count'].head(1).squeeze()
                     self.labels[name] = label
-                    self.counts[name] = count
+                    #self.counts[name] = count
                 
     def _csv_to_label_mask(self, path, img_size):
         mask = Image.new(mode='1', size=img_size, color=0)
@@ -72,76 +247,76 @@ class AnnotatedLoader():
                 self.image_len = (self.image_len[1]+1, min(self.image_len[1] + self.memory_limit, len(self.image_paths) - self.image_len[1]))
                 self._load_images()
             
-            img, lbl, count = self.images.pop(name), self.labels.pop(name), self.counts.pop(name)
+            img, lbl = self.images.pop(name), self.labels.pop(name)
+            self.length -= 1
             
-            if self.give_counts:
-                yield img, count
-            else:
-                yield img, lbl
+           
+            yield img, lbl
+                
+    def __len__(self):
+        return self.length
 
-loader = AnnotatedLoader()
-dataset = loader.data()
 
-if __name__ == '__main__':
+# if __name__ == '__main__':
     
-    train = False
+#     train = False
     
-    if train:
-        model = StarDist2D(config=Config2D(
-                n_channel_in=3
-            ), name='empty-model', basedir='test/model')
-        model.prepare_for_training()
+#     if train:
+#         model = StarDist2D(config=Config2D(
+#                 n_channel_in=3
+#             ), name='empty-model', basedir='test/model')
+#         model.prepare_for_training()
 
-        X_train = []
-        y_train = []
-        for _ in range(10):
-            X, y = next(dataset)
-            X_train.append(X)
-            y_train.append(y)
+#         X_train = []
+#         y_train = []
+#         for _ in range(10):
+#             X, y = next(dataset)
+#             X_train.append(X)
+#             y_train.append(y)
             
-        X_val = []
-        y_val = []
-        for _ in range(5):
-            X, y = next(dataset)
-            X_val.append(X)
-            y_val.append(y)
+#         X_val = []
+#         y_val = []
+#         for _ in range(5):
+#             X, y = next(dataset)
+#             X_val.append(X)
+#             y_val.append(y)
         
-    else:
-        model = StarDist2D(config=None, name='empty-model', basedir='test/model')
+#     else:
+#         model = StarDist2D(config=None, name='empty-model', basedir='test/model')
    
-    loader.give_counts = True
-    X_test = []
-    y_test = []
-    for _ in range(5):
-        X, y = next(dataset)
-        X_test.append(X)
-        y_test.append(y)
+#     loader.give_counts = True
+#     X_test = []
+#     y_test = []
+#     for _ in range(5):
+#         X, y = next(dataset)
+#         X_test.append(X)
+#         y_test.append(y)
     
-    if train:
-        loader.give_counts = False
-        X_train = np.array(X_train)
-        y_train = np.array(y_train)
+#     if train:
+#         loader.give_counts = False
+#         X_train = np.array(X_train)
+#         y_train = np.array(y_train)
         
-        X_val = np.array(X_val)
-        y_val = np.array(y_val)    
+#         X_val = np.array(X_val)
+#         y_val = np.array(y_val)    
         
-        print('Model is training...')
-        history = model.train(X_train, y_train, validation_data=(X_val, y_val), epochs=1)
-        model.
-        print('Model done training!')
+#         print('Model is training...')
+#         history = model.train(X_train, y_train, validation_data=(X_val, y_val), epochs=1)
+#         #model.
+#         print('Model done training!')
     
        
-    X_test = np.array(X_test)
-    y_test = np.array(y_test)
+#     X_test = np.array(X_test)
+#     y_test = np.array(y_test)
     
     
-    print('Model is predicting...')
-    for img in X_test:
-        mask, output = model.predict_instances(img, n_tiles=model._guess_n_tiles(img))
-        break
+#     print('Model is predicting...')
+#     for img in X_test:
+#         mask, output = model.predict_instances(img, n_tiles=model._guess_n_tiles(img))
+#         break
    
-    print('Model done predicting!')
+#     print('Model done predicting!')
     
-    print(len(output['points']), y_test[0])
-    print(output)
-    print(mask.shape)
+#     print(len(output['points']), y_test[0])
+#     print(output)
+#     print(mask.shape)
